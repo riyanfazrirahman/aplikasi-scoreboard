4265542012f38a8624b3ea506b9c61a178507805
riyanfazrirahman <riyanfazrirahman@gmail.com>
2024-09-11 22:31:01 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Aplikasi-Scoreboard
+
+Sebuah aplikasi dengan fitur login.
+
+Alur Fitur Login:
+
+1. Mengganti Nama tim.
+2. Menghitung score.
+3. Menampilkan Score.
---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Panggil fungsi handleChangeName
+  handleChangeName(1); // Untuk tim 1
+  handleChangeName(2); // Untuk tim 2
+
+  // Panggil fungsi untuk skor
+  handleScoreCounter(1); // Untuk tim 1
+  handleScoreCounter(2); // Untuk tim 2
+});
